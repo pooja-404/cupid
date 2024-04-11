127da9ca3a76c4af5a45e3095d1cb83283103da0
pooja-404 <pk3924792@gmail.com>
2024-04-11 10:41:10 +0530

    some change

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -12,7 +12,7 @@ const Faq = () => {
         <div id='Faqs' className='container max-w-[1300px]'>
             <div className='max-w-[700px] mx-auto pt-16 sm:pt-32 md:pt-52 lg:pt-64'>
                 <h2 className='lg:text-4xl sm:text-3xl2 text-3xl1 font-bold tracking-tighter text-white leading-11'>FAQs</h2>
-                <p className='pb-2 mt-5 text-lg leading-6 text-gray font-inter sm:text-xl text-lg md:leading-7 tracking-tightest md:mt-6'>Answers to the most frequently asked questions.</p>
+                <p className='pb-2 mt-5 font-normal leading-6 text-gray font-inter sm:text-xl text-lg md:leading-7 tracking-tightest md:mt-6'>Answers to the most frequently asked questions.</p>
                 {accordionData.map((item, index) => (
                     <Accordion key={index} onClick={() => toggleAccordion(index)} className={`cursor-pointer border-b border-white border-opacity-10 mt-5 sm:mt-6 md:mt-8 ${item.id === 6 ? "border-0" : ""}`} open={open === index} icon={<Arrow id={index} open={open} />}>
                         <AccordionHeader className={`font-semibold text-white text-2sm md:text-base text-start w-full justify-between gap-3 border-0  mb-0.5 pt-0 duration-500 leading-6 pb-5 sm:pb-6 md:pb-8 focus-visible:outline-none ${open === index ? "" : ""}`}>{item.title}
